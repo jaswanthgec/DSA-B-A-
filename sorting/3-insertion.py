983fbb0e6122271200ec46a76bf562eb8fbcee22
Jaswanth Kollipara <100593255+jaswanthgec@users.noreply.github.com>
2024-07-16 22:40:26 +0530

    1.0

---
+++ sorting/3-insertion.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# Example usage:
+arr = [12, 11, 13, 5, 6]
+print("Insertion Sort:", insertion_sort(arr))
